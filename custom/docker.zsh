5fbd663db8860c457b4bc45b7b65897a21f9dce9
ryanbillingsley <ryanbbillingsley@gmail.com>
2015-03-09 09:45:17 -0400

    Added some more env stuff

---
+++ custom/docker.zsh
@@ -0,0 +1,4 @@
+export DOCKER_CERT_PATH=/Users/ryanbillingsley/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_IP=$(boot2docker ip 2>/dev/null)
--- custom/go.zsh
+++ custom/go.zsh
@@ -1,2 +1,2 @@
-export GOPATH=$PROJECTS/go
+export GOPATH=$HOME/Dev/go
 export PATH="$GOPATH/bin:$PATH"
